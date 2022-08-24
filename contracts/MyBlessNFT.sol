a0cf012706d5b12504abe07dd18c41ae958a11b0
maxyou <hyyou2010@gmail.com>
2022-08-24 17:20:44 +0800

    imgur+jsonkeeper ok

--- contracts/MyBlessNFT.sol
+++ contracts/MyBlessNFT.sol
@@ -7,15 +7,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-// We need to import the helper functions from the contract that we copy/pasted.
-import { Base64 } from "./libraries/Base64.sol";
 
 contract MyBlessNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
-
-
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
   // event NFTbless(address sender, string bless);
   // event NFTMark(address sender, string mark);
@@ -34,43 +29,12 @@ contract MyBlessNFT is ERC721URIStorage {
 
     uint256 newItemId = _tokenIds.current();
 
-    //emit NFTbless(msg.sender, bless);
-    string memory mark = string(abi.encodePacked(bless));
-    //emit NFTMark(msg.sender, mark);
-
-    string memory finalSvg = string(abi.encodePacked(baseSvg, mark,  "</text></svg>"));
-    // string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "-", Strings.toString(newItemId),  "</text></svg>"));
-
-    // Get all the JSON metadata in place and base64 encode it.
-    string memory json = Base64.encode(
-        bytes(
-            string(
-                abi.encodePacked(
-                    '{"name": "',
-                    // We set the title of our NFT as the generated word.
-                    Strings.toString(newItemId+1),
-                    '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
-                    // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
-                    Base64.encode(bytes(finalSvg)),
-                    '"}'
-                )
-            )
-        )
-    );
-
-    // Just like before, we prepend data:application/json;base64, to our data.
-    string memory finalTokenUri = string(
-        abi.encodePacked("data:application/json;base64,", json)
-    );
-
-    console.log("\n--------------------");
-    console.log(finalTokenUri);
-    console.log("--------------------\n");
-
+    string memory json = 'https://jsonkeeper.com/b/TYL7';    
+    
     _safeMint(msg.sender, newItemId);
     
     // Update your URI!!!
-    _setTokenURI(newItemId, finalTokenUri);
+    _setTokenURI(newItemId, json);
   
     _tokenIds.increment();
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
